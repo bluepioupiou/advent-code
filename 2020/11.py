c60a920c3e4aac0096a6fd514bee3295b7e00d69
Alain <bluepioupiou@gmail.com>
2020-12-11 21:54:03 +0100

    debut du 11

---
+++ 2020/11.py
@@ -0,0 +1,36 @@
+import itertools
+file = open('11.txt', 'r')
+Lines = file.readlines()
+
+# Strips the newline character
+seats = [[seat for seat in line.strip()] for line in Lines]
+print("".join(["".join(row) for row in seats]))
+print("{}".format(seats))
+while True:
+    result = []
+    for i, row in enumerate(seats):
+        new_row = []
+        for j, seat in enumerate(row):
+            occupied = 0
+            for x, y in list(itertools.product(range(-1, 2), range(-1, 2))):
+                if x != 0 and y != 0:
+                    if 0 < i + x < len(seats) and 0 < j + y < len(row):
+                        neighbour = seats[i+x][j+y]
+                        if neighbour == "#":
+                            occupied += 1
+            if seat == "#" and occupied >= 4:
+                new_row.append("L")
+            elif seat == "L" and occupied == 0:
+                new_row.append("#")
+            else:
+                new_row.append(seat)
+
+        result.append(new_row)
+
+    #print("from \n{}".format("\n".join(["".join(row) for row in seats])))
+    print("to \n{}".format("\n".join(["".join(row) for row in result])))
+    if "".join(["".join(row) for row in result]) == "".join(["".join(row) for row in seats]):
+        break
+    else:
+        seats = result
+print("found {}".format(sum([row.count("#") for row in result])))
