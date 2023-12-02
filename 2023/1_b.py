e9577ed7429f514828dbf6c398cfe2f5ad0d619a
Alain <bluepioupiou@gmail.com>
2023-12-02 10:22:30 +0100

    day 2

--- 2023/1_b.py
+++ 2023/1_b.py
@@ -13,14 +13,28 @@ words = {
 }
 total = 0
 for line in lines:
-    print(line)
+    first = None
+    current = None
+    last = None
+    #print(line)
     for i in range(len(line)):
         for word, digit in words.items():
             if line[i::].startswith(word):
-                line = line.replace(word, digit)
-    print(line)
-    only_decimals = [c for c in line if c.isdecimal()]
-    number = int(f"{only_decimals[0]}{only_decimals[-1]}")
+                #print(f"---found word {word}")
+                current = words[word]
+                break
+        else:
+            if line[i].isdecimal():
+                #print(f"---found digit {line[i]}")
+                current = line[i]
+        if current and not first:
+            first = current
+        if current:
+            last = str(current)
+        current = None
+        #print(f"{first}-{current}-{last}")
+
+    number = f"{first}{last}"
     print(number)
-    total += number
+    total += int(number)
 print(total)
