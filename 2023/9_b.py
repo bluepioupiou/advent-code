0ab3ce4a296c3ee6db774e75939efa05179267df
Alain <bluepioupiou@gmail.com>
2023-12-10 23:19:36 +0100

    day 9

---
+++ 2023/9_b.py
@@ -0,0 +1,26 @@
+import re
+import functools
+file = open('9.txt', 'r')
+lines = file.readlines()
+
+total = 0
+for line in lines:
+    values = [int(x) for x in re.findall('-?\d+', line)]
+    sequences = [values]
+    while values and len(list(filter(lambda x: x != 0, values))):
+        print(values)
+        new_values = []
+        for step_x, step_y in zip(values[0:-1], values[1:]):
+            #print(step_x, step_y)
+            new_values.append(step_y - step_x)
+        values = new_values
+        if len(values):
+            sequences.append(values)
+
+    extrapolate = 0
+    for sequence in list(reversed(sequences)):
+        extrapolate = sequence[0] - extrapolate
+    print(f"Extrapolate: {extrapolate}")
+    total += extrapolate
+
+print(f"Solution {total}")
