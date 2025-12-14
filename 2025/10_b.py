ee5343043b74ffcfe6784721ac1a11a2da6b0332
Alain Begey <begey.alain@gmail.com>
2025-12-15 00:17:38 +0100

    réussite du jour 10b grace aux solvers python

--- 2025/10_b.py
+++ 2025/10_b.py
@@ -1,5 +1,6 @@
 import re
 from utils import log
+from z3 import *
 
 if __name__ == '__main__':
     file = open('10.txt', 'r')
@@ -7,36 +8,32 @@ if __name__ == '__main__':
     result = 0
     for line in lines:
         target_light = re.search(r"\[(.*?)\]", line).group(1)
-        buttons = [tuple(map(int, x.split(","))) for x in re.findall(r"\((.*?)\)", line)]
-        target_voltage = tuple(int(x) for x in re.search(r"\{(.*?)\}", line).group(1).split(","))
-        log(f"target_light {target_light}, buttons {buttons}, target_voltages {target_voltage}({len(target_voltage)})")
-        current_voltages = {tuple([0] * len(target_voltage))}
-        log(f"starting voltages {current_voltages}")
-        iteration = 0
-        while True:
-            new_voltages = set()
-            iteration += 1
-            for initial_current_voltage in current_voltages:
-                for button in buttons:
-                    current_voltage = initial_current_voltage
-                    for position in button:
-                        current_voltage = (*current_voltage[:position], current_voltage[position] + 1, *current_voltage[position + 1:])
-                    #log(f" - with button {button}: {initial_current_voltage} > {current_voltage} ... {target_voltage}")
-                    if current_voltage == target_voltage:
-                        # On a trouvé !
-                        log(f"at iteration {iteration}: found {current_voltage} from {initial_current_voltage} with button {button}")
-                        break
-                    new_voltages.add(current_voltage)
-                else:
-                    continue
-                break
-            else:
-                current_voltages = new_voltages
-                log(f"at iteration {iteration}: {len(current_voltages)}")
-                continue
-            #log(f"found min iteration {iteration}")
-            result += iteration
-            break
+        buttons = [list(map(int, x.split(",")))
+           for x in re.findall(r"\((.*?)\)", line)]
+        vectors = []
+        for button in buttons:
+            #log(f" - build button {button} for len {len(target_light)}")
+            vectors.append([1 if i in button else 0 for i in range(len(target_light))])
+        target_voltage = [int(x) for x in re.search(r"\{(.*?)\}", line).group(1).split(",")]
+        #log(f"target_light {target_light}, vectors {vectors}, target_voltages {target_voltage}({len(target_voltage)})")
 
+        s = Solver()
+        x = [Int(f"x_{i}") for i in range(len(vectors))]
+
+        for i in range(len(vectors)):
+            s.add(x[i] >= 0)
+
+        for j in range(len(target_voltage)):
+            s.add(
+                Sum([x[i] * vectors[i][j] for i in range(len(vectors))]) == target_voltage[j]
+            )
+
+        opt = Optimize()
+        opt.add(s.assertions())
+        opt.minimize(Sum(x))
+
+        if opt.check() == sat:
+            m = opt.model()
+            result += sum([m[x[i]].as_long() for i in range(len(vectors))])
     print(result)
 
